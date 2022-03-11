3abf6ebbc6edd5fd603848b323821d99f7cf4e17
Rico <guinanaorico@gmail.com>
2022-03-11 19:35:58 +0800

    Update lot register

--- Admin/queries/block-select.php
+++ Admin/queries/block-select.php
@@ -6,11 +6,11 @@ include("../../config.php");
 $con=connect();
 extract($_POST);
 
-$block_lot=$con->query("SELECT * FROM `tbl_blocks` WHERE `site_id`='$site_id'");
+$block_lot=$con->query("SELECT * FROM `tbl_blocks` WHERE `site_id`='$site_id' AND `sector`='$sector' ORDER BY `block_name` ASC");
 if($block_lot->num_rows!=0){
   while($row=$block_lot->fetch_array()){
     echo "<option value=".$row['block_id'].">".$row['block_name']."</option>";
   }
 }else{
-    echo "<option value='' selected disabled>Select Site First</option>";
+    echo "<option value='' selected disabled>No block available</option>";
 }
--- Admin/queries/block_and_lot.php
+++ Admin/queries/block_and_lot.php
@@ -35,38 +35,71 @@ if(isset($_POST["btn-submit-block"])){
   $sector=mysqli_real_escape_string($con, $_POST["sector"]);
   $site_id=mysqli_real_escape_string($con, $_POST["site-id"]);
 
-  $sql=$con->query("INSERT INTO `tbl_blocks`(`site_id`, `block_name`, `sector`, `total_lots`) VALUES ('$site_id','$block_name', '$sector','0')");
+  $sql=$con->query("SELECT * FROM `tbl_blocks` WHERE `block_name`='$block_name' AND `sector`='$sector' AND `site_id` ='$site_id'");
+  $row_block=$sql->fetch_array();
 
-  $sql=$con->query("SELECT COUNT(`block_id`) AS `blocks` FROM `tbl_blocks` WHERE `site_id`='$site_id'");
-  $row=$sql->fetch_array();
-  $total_blocks=$row["blocks"];
+  if($block_name!=isset($row_block["block_name"])&&$sector!=isset($row_block["sector"])&&$site_id!=isset($row_block["site_id"])){
 
-  $sql=$con->query("UPDATE `tbl_sites` SET `total_blocks`='$total_blocks' WHERE `site_id`='$site_id'");
+    $sql=$con->query("INSERT INTO `tbl_blocks`(`site_id`, `block_name`, `sector`, `total_lots`) VALUES ('$site_id','$block_name', '$sector','0')");
 
-  echo "<script>
-  Swal.fire({
-    position: 'center',
-    icon: 'success',
-    title: 'Block Successfully Added',
-    text: 'You added a new block',
-    showConfirmButton: false,
-    timer: 2000,
-    allowOutsideClick: () => {
-      const popup = Swal.getPopup()
-      popup.classList.remove('swal2-show')
-      setTimeout(() => {
-        popup.classList.add('animate__animated', 'animate__headShake')
-      })
-      setTimeout(() => {
-        popup.classList.remove('animate__animated', 'animate__headShake')
-      }, 500)
-      return false
-    }
-  })
-  window.history.replaceState( null, null, window.location.href );
-  </script>";
+    $sql=$con->query("SELECT COUNT(`block_id`) AS `blocks` FROM `tbl_blocks` WHERE `site_id`='$site_id'");
+    $row=$sql->fetch_array();
+    $total_blocks=$row["blocks"];
+  
+    $sql=$con->query("UPDATE `tbl_sites` SET `total_blocks`='$total_blocks' WHERE `site_id`='$site_id'");
+  
+    echo "<script>
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Block Successfully Added',
+      text: 'You added a new block',
+      showConfirmButton: false,
+      timer: 2000,
+      allowOutsideClick: () => {
+        const popup = Swal.getPopup()
+        popup.classList.remove('swal2-show')
+        setTimeout(() => {
+          popup.classList.add('animate__animated', 'animate__headShake')
+        })
+        setTimeout(() => {
+          popup.classList.remove('animate__animated', 'animate__headShake')
+        }, 500)
+        return false
+      }
+    })
+    window.history.replaceState( null, null, window.location.href );
+    </script>";
+  
+    header("refresh: 1;");
 
-  header("refresh: 1;");
+  }else{
+    echo "<script>
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: 'Block Already Exists',
+      text: 'Enter other block number',
+      showConfirmButton: false,
+      timer: 2000,
+      allowOutsideClick: () => {
+        const popup = Swal.getPopup()
+        popup.classList.remove('swal2-show')
+        setTimeout(() => {
+          popup.classList.add('animate__animated', 'animate__headShake')
+        })
+        setTimeout(() => {
+          popup.classList.remove('animate__animated', 'animate__headShake')
+        }, 500)
+        return false
+      }
+    })
+    window.history.replaceState( null, null, window.location.href );
+    </script>";
+  
+    header("refresh: 1;");
+  }
+
 }
 if(isset($_POST["btn-submit-lot"])){
   $site_id=mysqli_real_escape_string($con, $_POST["site-lot"]);
