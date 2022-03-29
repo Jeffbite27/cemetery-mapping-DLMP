4d10b0133384b7802a7b3b7bc0ab9fb965cf2527
Rico <guinanaorico@gmail.com>
2022-03-30 00:02:55 +0800

    Added grave map

---
+++ Admin/grave-map.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="../Assets/image/logopngplain.png" type="image/x-icon">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Grave Map | Divine Life Memorial Park</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../Assets/DataTables/datatables.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../Assets/css/index_admin.css">
+    
+    <script src="../Assets/js/sweetalert.js"></script>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div class="sidebar close">
+
+    <div class="logo-details">
+      <img src="../Assets/image/logopngplain.png" alt="">
+    </div>
+
+    <ul class="nav-links">
+      
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+        <a href="dashboard.php">
+          <i class='bx bx-grid-alt bx-rotate-180' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customer">
+        <a href="customer.php">
+          <i class='bx bx-user' ></i>
+          <span class="link_name">Customer</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Block & Lot Setup">
+        <a href="block-and-lot.php">
+          <i class='bx bx-layer'></i>
+          <span class="link_name">Block & Lot Setup</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Interment Setup">
+        <a href="interment_Setup.php">
+          <i class='bx bxs-user-rectangle' ></i>
+          <span class="link_name">Interment Setup</span>
+        </a>
+      </li>
+
+      <li class="tabs active" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Grave Map">
+        <a href="grave-map.php">
+          <i class='bx bx-map-alt' ></i>
+          <span class="link_name">Grave Map</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Reports">
+        <a href="#">
+          <i class='bx bx-bar-chart-square'></i>
+          <span class="link_name">Reports</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Slideshow">
+        <a href="#">
+          <i class='bx bx-carousel' ></i>
+          <span class="link_name">Slideshow</span>
+        </a>
+      </li>
+
+      <li class="tabs" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="News & Events">
+        <a href="#">
+          <i class='bx bx-news'></i>
+          <span class="link_name">News & Events</span>
+        </a>
+      </li>
+      
+    </ul>
+
+  </div>
+  
+</body>
+</html>
