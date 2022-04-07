4893984e7f73250acc370a8b8716eece7ae1c70a
Rico <guinanaorico@gmail.com>
2022-04-08 01:02:28 +0800

    Change order by block name to id

--- Admin/queries/block-select.php
+++ Admin/queries/block-select.php
@@ -6,7 +6,7 @@ include("../../config.php");
 $con=connect();
 extract($_POST);
 
-$block_lot=$con->query("SELECT * FROM `tbl_blocks` WHERE `site_id`='$site_id' AND `sector`='$sector' ORDER BY `block_name` ASC");
+$block_lot=$con->query("SELECT * FROM `tbl_blocks` WHERE `site_id`='$site_id' AND `sector`='$sector' ORDER BY `block_id` ASC");
 if($block_lot->num_rows!=0){
   while($row=$block_lot->fetch_array()){
     echo "<option value=".$row['block_id'].">".$row['block_name']."</option>";
