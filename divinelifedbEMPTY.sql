d3adda6f72b2c64ece6aadb444e39395b51b6fd0
Rico <guinanaorico@gmail.com>
2022-04-08 01:01:05 +0800

    EMPTY DB

---
+++ divinelifedbEMPTY.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2022 at 06:23 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `divinelifedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_acc`
+--
+
+CREATE TABLE `admin_acc` (
+  `id` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_acc`
+--
+
+INSERT INTO `admin_acc` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `family_name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `bday` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `religion` varchar(255) NOT NULL,
+  `citizenship` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `work` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deceased_persons`
+--
+
+CREATE TABLE `deceased_persons` (
+  `deceased_id` int(11) NOT NULL,
+  `lot_owner_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `block_id` int(11) NOT NULL,
+  `lot_id` int(11) NOT NULL,
+  `dead_family_name` varchar(255) NOT NULL,
+  `dead_fname` varchar(255) NOT NULL,
+  `dead_mname` varchar(255) NOT NULL,
+  `dead_gender` varchar(255) NOT NULL,
+  `dead_citizenship` varchar(255) NOT NULL,
+  `dead_civil_status` varchar(255) NOT NULL,
+  `dead_relative` varchar(255) NOT NULL,
+  `dead_relative_surname` varchar(255) NOT NULL,
+  `dead_relationship` varchar(255) NOT NULL,
+  `internment_date` varchar(255) NOT NULL,
+  `date_of_birth` varchar(255) NOT NULL,
+  `date_of_death` varchar(255) NOT NULL,
+  `death_cert` varchar(255) NOT NULL,
+  `burial_permit` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lot_owners`
+--
+
+CREATE TABLE `lot_owners` (
+  `lot_owner_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `block_id` int(11) NOT NULL,
+  `lot_id` int(11) NOT NULL,
+  `deed_of_sale` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_blocks`
+--
+
+CREATE TABLE `tbl_blocks` (
+  `block_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `block_name` varchar(255) NOT NULL,
+  `sector` varchar(255) NOT NULL,
+  `total_lots` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_lots`
+--
+
+CREATE TABLE `tbl_lots` (
+  `lot_id` int(11) NOT NULL,
+  `block_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `lot_name` varchar(255) NOT NULL,
+  `lawn_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sites`
+--
+
+CREATE TABLE `tbl_sites` (
+  `site_id` int(11) NOT NULL,
+  `site_name` varchar(255) NOT NULL,
+  `site_sqm2` varchar(255) NOT NULL,
+  `total_blocks` varchar(255) NOT NULL,
+  `total_lots` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `deceased_persons`
+--
+ALTER TABLE `deceased_persons`
+  ADD PRIMARY KEY (`deceased_id`);
+
+--
+-- Indexes for table `lot_owners`
+--
+ALTER TABLE `lot_owners`
+  ADD PRIMARY KEY (`lot_owner_id`);
+
+--
+-- Indexes for table `tbl_blocks`
+--
+ALTER TABLE `tbl_blocks`
+  ADD PRIMARY KEY (`block_id`);
+
+--
+-- Indexes for table `tbl_lots`
+--
+ALTER TABLE `tbl_lots`
+  ADD PRIMARY KEY (`lot_id`);
+
+--
+-- Indexes for table `tbl_sites`
+--
+ALTER TABLE `tbl_sites`
+  ADD PRIMARY KEY (`site_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `deceased_persons`
+--
+ALTER TABLE `deceased_persons`
+  MODIFY `deceased_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lot_owners`
+--
+ALTER TABLE `lot_owners`
+  MODIFY `lot_owner_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_blocks`
+--
+ALTER TABLE `tbl_blocks`
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_lots`
+--
+ALTER TABLE `tbl_lots`
+  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_sites`
+--
+ALTER TABLE `tbl_sites`
+  MODIFY `site_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
