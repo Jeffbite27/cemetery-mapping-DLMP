f6cbb57dcd8cd6ae73f7c3c6eb9c0d320dd4be57
Rico <guinanaorico@gmail.com>
2022-04-22 18:50:58 +0800

    Deleted update of total block and lots

--- Admin/queries/block_and_lot.php
+++ Admin/queries/block_and_lot.php
@@ -73,19 +73,6 @@ if(isset($_POST["btn-update-site"])){
   if($site_name!=isset($row["site_name"])){
     $sql=$con->query("UPDATE `tbl_sites` SET `site_name`='$site_name', `site_sqm2`='$square_meters' WHERE `site_id`='$site_id'");
 
-    //COUNT BLOCKS
-    $sql=$con->query("SELECT COUNT(`block_id`) AS `blocks` FROM `tbl_blocks` WHERE `site_id`='$site_id'");
-    $row=$sql->fetch_array();
-    $total_blocks=$row["blocks"];
-  
-     //COUNT UPDATE BLOCKS
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_blocks`='$total_blocks' WHERE `site_id`='$site_id'");
-
-    //UPDATE TOTAL LOTS IN SITE
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_lots`='$total_lots' WHERE `site_id`='$site_id'");
-
-    $sql=$con->query("UPDATE `tbl_blocks` SET `total_lots`='$total_lots_block' WHERE `block_id`='$block_id'");
-
     echo "<script>
     Swal.fire({
       position: 'center',
@@ -149,12 +136,6 @@ if(isset($_POST["btn-submit-block"])){
 
     $sql=$con->query("INSERT INTO `tbl_blocks`(`site_id`, `block_name`, `sector`, `total_lots`) VALUES ('$site_id','$block_name', '$sector','0')");
 
-    $sql=$con->query("SELECT COUNT(`block_id`) AS `blocks` FROM `tbl_blocks` WHERE `site_id`='$site_id'");
-    $row=$sql->fetch_array();
-    $total_blocks=$row["blocks"];
-  
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_blocks`='$total_blocks' WHERE `site_id`='$site_id'");
-  
     echo "<script>
     Swal.fire({
       position: 'center',
@@ -219,14 +200,6 @@ if(isset($_POST["btn-update-block"])){
 
   if($block_name!=isset($row["block_name"])&&$sector!=isset($row["sector"])){
     $sql=$con->query("UPDATE `tbl_blocks` SET `block_name`='$block_name', `sector`='$sector' WHERE `block_id`='$block_id'");
-
-    //COUNT BLOCKS
-    $sql=$con->query("SELECT COUNT(`block_id`) AS `blocks` FROM `tbl_blocks` WHERE `site_id`='$site_id'");
-    $row=$sql->fetch_array();
-    $total_blocks=$row["blocks"];
-  
-     //COUNT UPDATE BLOCKS
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_blocks`='$total_blocks' WHERE `site_id`='$site_id'");
 
     echo "<script>
     Swal.fire({
@@ -294,17 +267,6 @@ if(isset($_POST["btn-submit-lot"])){
 
     $sql=$con->query("INSERT INTO `tbl_lots`(`block_id`, `site_id`, `lot_name`, `lawn_type`) VALUES ('$block_id','$site_id','$lot_name','$lawn_type')");
   
-    $sql=$con->query("SELECT COUNT(`lot_id`) AS `lots` FROM `tbl_lots` WHERE `site_id`='$site_id'");
-    $row=$sql->fetch_array();
-    $total_lots=$row["lots"];
-
-    $sql=$con->query("SELECT COUNT(`lot_id`) AS `lots_block` FROM `tbl_lots` WHERE `block_id`='$block_id'");
-    $row=$sql->fetch_array();
-    $total_lots_block=$row["lots_block"];
-
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_lots`='$total_lots' WHERE `site_id`='$site_id'");
-    $sql=$con->query("UPDATE `tbl_blocks` SET `total_lots`='$total_lots_block' WHERE `block_id`='$block_id'");
-
     echo "<script>
     Swal.fire({
       position: 'center',
@@ -369,20 +331,6 @@ if(isset($_POST["btn-update-lot"])){
 
   if($lot_name!=isset($row["lot_name"])&&$block_id!=isset($row["block_id"])){
     $sql=$con->query("UPDATE `tbl_lots` SET `lot_name`='$lot_name',`lawn_type`='$lawn_type' WHERE `lot_id`='$lot_id'");
-
-     //UPDATE TOTAL LOTS IN BLOCKS
-    $sql=$con->query("SELECT COUNT(`lot_id`) AS `lots` FROM `tbl_lots` WHERE `site_id`='$site_id'");
-    $row=$sql->fetch_array();
-    $total_lots=$row["lots"];
-
-    $sql=$con->query("SELECT COUNT(`lot_id`) AS `lots_block` FROM `tbl_lots` WHERE `block_id`='$block_id'");
-    $row=$sql->fetch_array();
-    $total_lots_block=$row["lots_block"];
-
-    //UPDATE TOTAL LOTS IN SITE
-    $sql=$con->query("UPDATE `tbl_sites` SET `total_lots`='$total_lots' WHERE `site_id`='$site_id'");
-
-    $sql=$con->query("UPDATE `tbl_blocks` SET `total_lots`='$total_lots_block' WHERE `block_id`='$block_id'");
 
     echo "<script>
     Swal.fire({
